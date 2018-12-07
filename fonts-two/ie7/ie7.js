9d9049ddb429a7800732644c6267dfaccc710b4b
zouliang <184051430@qq.com>
2018-12-07 20:08:21 +0800

    新增音量栏

---
+++ fonts-two/ie7/ie7.js
@@ -0,0 +1,39 @@
+/* To avoid CSS expressions while still supporting IE 7 and IE 6, use this script */
+/* The script tag referencing this file must be placed before the ending body tag. */
+
+/* Use conditional comments in order to target IE 7 and older:
+	<!--[if lt IE 8]><!-->
+	<script src="ie7/ie7.js"></script>
+	<!--<![endif]-->
+*/
+
+(function() {
+	function addIcon(el, entity) {
+		var html = el.innerHTML;
+		el.innerHTML = '<span style="font-family: \'iconZl\'">' + entity + '</span>' + html;
+	}
+	var icons = {
+		'icon-share_fill': '&#xe729;',
+		'icon-systemprompt_fill': '&#xe72f;',
+		'icon-trash': '&#xe738;',
+		'icon-bofang': '&#xe624;',
+		'icon-zanting': '&#xe629;',
+		'icon-faxian': '&#xe631;',
+		'icon-sousuo': '&#xe632;',
+		'icon-pingjia': '&#xe641;',
+		'0': 0
+		},
+		els = document.getElementsByTagName('*'),
+		i, c, el;
+	for (i = 0; ; i += 1) {
+		el = els[i];
+		if(!el) {
+			break;
+		}
+		c = el.className;
+		c = c.match(/icon-[^\s'"]+/);
+		if (c && icons[c[0]]) {
+			addIcon(el, icons[c[0]]);
+		}
+	}
+}());
--- js/index.js
+++ js/index.js
@@ -1,54 +1,96 @@
 $(function () {
-   let video = $('video').get(0);
-   video.addEventListener('canplay',()=>{//当视频准备好播放时
-       $('video').css('display','block');
-       //显示视频的时间
-       let videoTime = getVideoTime(video.duration) ;
-        $('.whole-time').text(videoTime)
-   });
-   $('.switch').on('click',function () {
-       if($(this).hasClass('itlike-play3')){
-           $(this).removeClass('itlike-play3').addClass('itlike-pause2');
-           //播放
-           video.play();
+    let video = $('video').get(0)
+   initVideo(video);
+   initVolume(video);
+   /*初始化音乐播放*/
+   function initVideo(obj) {
+       let video = obj;
+       video.addEventListener('canplay',()=>{//当视频准备好播放时
+           $('video').css('display','block');
+           //显示视频的时间
+           let videoTime = getVideoTime(video.duration) ;
+           $('.whole-time').text(videoTime)
+       });
+       $('.switch').on('click',function () {
+           if($(this).hasClass('itlike-play3')){
+               $(this).removeClass('itlike-play3').addClass('itlike-pause2');
+               //播放
+               video.play();
+               video.volume = 0.5;
+              console.log(video.volume)
+           }else {
+               $(this).removeClass('itlike-pause2').addClass('itlike-play3');
+               video.pause()
+           }
+       });
+       video.addEventListener('timeupdate',()=>{
+           /*     console.log(video.currentTime);*/
+           let currentTime = getVideoTime(video.currentTime);
+           $('.current-time').text(currentTime);
+           //计时条动起来
+           let widthProgress =  (video.currentTime/video.duration) * $('.progress').width() ;
+           /* console.log(widthProgress)*/
+           $('.progress-line').css('width',widthProgress)
 
-       }else {
-           $(this).removeClass('itlike-pause2').addClass('itlike-play3');
-           video.pause()
+       });
+       video.addEventListener('ended',()=>{
+           video.currentTime = 0;
+           $('.switch').removeClass('itlike-pause2').addClass('itlike-play3')
+
+       });
+       $('.fullScreen').on('click',()=>{
+           if( $('.fullScreen').hasClass('itlike-enlarge')){
+               video.webkitRequestFullScreen()
+           }else {
+               window.webkitCancelFullScreen()
+           }
+       });
+       $('.progress-bar').on('click',function (e) {
+           let clickW = e.offsetX;
+         /*  console.log(e.target);*/
+           let updateT = (clickW/$('.progress').width() )* video.duration;
+           video.currentTime = updateT
+       });
+       function getVideoTime(time) {
+           let h = Math.floor(time/(60*60));
+           let m = Math.floor(time/60%60);
+           let s = Math.floor(time%60);
+           return  ( h < 10 ? '0'+ h : h )+ ':' + (m < 10 ? '0'+ m : m) +':'+  (s < 10 ? '0'+ s : s)
        }
-   });
-   video.addEventListener('timeupdate',()=>{
-  /*     console.log(video.currentTime);*/
-       let currentTime = getVideoTime(video.currentTime);
-       $('.current-time').text(currentTime);
-       //计时条动起来
-       let widthProgress =  (video.currentTime/video.duration) * $('.progress').width() ;
-      /* console.log(widthProgress)*/
-       $('.progress-line').css('width',widthProgress)
-
-   });
-   video.addEventListener('ended',()=>{
-       video.currentTime = 0;
-       $('.switch').removeClass('itlike-pause2').addClass('itlike-play3')
-
-   });
-    $('.fullScreen').on('click',()=>{
-        if( $('.fullScreen').hasClass('itlike-enlarge')){
-            video.webkitRequestFullScreen()
-        }else {
-          window.webkitCancelFullScreen()
-        }
-    });
-    $('.progress-bar').on('click',function (e) {
-       let clickW = e.offsetX;
-       let updateT = (clickW/$('.progress').width() )* video.duration;
-       video.currentTime = updateT
-    });
-   function getVideoTime(time) {
-       let h = Math.floor(time/(60*60));
-       let m = Math.floor(time/60%60);
-       let s = Math.floor(time%60);
-       return  ( h < 10 ? '0'+ h : h )+ ':' + (m < 10 ? '0'+ m : m) +':'+  (s < 10 ? '0'+ s : s)
    }
+       /*初始化音量*/
+      function initVolume(obj) {
+          let video = obj;
+          let voiceBtn = $('.voice-btn').get(0);
+          let voicePlay = $('.play-icon').get(0);
+          let voiceVal=$('.voice-value').get(0);
+          let voiceBackG = $('.voice-control-box').get(0);
+          let voiceBG = $('.voice-bg').get(0);
+          let clientH = 0;
+          let playH = 0;
+          $(voicePlay).css('height',56);
+          $(voiceBtn).css('bottom', 51);
+          $(voiceVal).text(50 + '%');
+          $(voiceBG).on('click',function (e){
+              if (e.stopPropagation) {
+                  e.stopPropagation()
+              } else {
+                  e.cancelBubble = true
+              }
+                    console.log(e.target);
+                    voiceBackG.parentNode.style.display = 'block';
+                    clientH = e.offsetY;
+                    let bgHeight = voiceBackG.offsetHeight;
+                    playH = bgHeight - clientH;
+                    let voiceValue = parseInt(((bgHeight - clientH) / bgHeight) * 100) + '%';
+                    $(voicePlay).css('height', playH);
+                    $(voiceBtn).css('bottom', playH - 5);
+                    $(voiceVal).text(voiceValue);
+                    video.volume = ((bgHeight - clientH) / bgHeight).toFixed(1);
+                    console.log(video.volume);
+                    clientH = 0;
+                    playH = 0;
+          });
 
+      }
 });
